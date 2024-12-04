643b8763e9ab817c8e071506c95ea714aa4d9eaf
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-04 15:44:44 -0300

    feat: add create task route

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -30,3 +30,48 @@ export const getTasks = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ error: 'Error retrieving tasks' });
   }
 };
+
+export const createTask = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const {
+    title,
+    description,
+    status,
+    priority,
+    tags,
+    startDate,
+    dueDate,
+    points,
+    projectId,
+    authorUserId,
+    assignedUserId,
+  } = req.body;
+
+  try {
+    const task = await prisma.task.create({
+      data: {
+        title,
+        description,
+        status,
+        priority,
+        tags,
+        startDate,
+        dueDate,
+        points,
+        projectId: Number(projectId),
+        authorUserId: Number(authorUserId),
+        assignedUserId: Number(assignedUserId),
+      },
+    });
+
+    res.status(201).json(task);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: `Error creating task: ${error.message}` });
+    }
+
+    res.status(500).json({ error: 'Error creating task' });
+  }
+};
