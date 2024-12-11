f90fd3fa78c3f228e0d8be413903400f3dc7d474
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-11 11:22:21 -0300

    chore: server config modifications

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ app.use('/users', userRoutes);
 app.use('/teams', teamRoutes);
 
 /* server */
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 8080;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
