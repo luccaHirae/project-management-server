958aa62aa0d7f2a211d920027d944e5256b8d60c
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-11 10:12:20 -0300

    fix: get user tasks route path

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -11,6 +11,6 @@ const router = Router();
 router.get('/', getTasks);
 router.post('/', createTask);
 router.patch('/:taskId/status', updateTaskStatus);
-router.get('/:userId', getUserTasks);
+router.get('/user/:userId', getUserTasks);
 
 export default router;
